95e48a27809ca4c89e53f033d26ac02ebda5f830
Phil Helm <philip.helm@omgplc.com>
2017-02-16 15:59:46 +0000

    results in B

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ module.exports = (path) => {
   return new Promise((res, rej) => {
     exec('du', ['-ks', path], (err, stdout, stderr) => {
       let matches = stdout.match(regex);
+      let kbytes = matches[1];
+      let bytes = kb * 1024;
       if (!err && !stderr && matches && matches.length > 1){
-        res(matches[1]);
+        res(bytes);
       } else {
         rej(err, stderr, stdout);
       }
